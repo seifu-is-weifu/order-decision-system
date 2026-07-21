c23f38b92965fc2db462273229dc48b866386492
Seif Abdulazim <seifu@Seifs-MacBook-Pro.local>
2026-07-22 01:33:25 +0300

    Create DynamoDB infrastructure

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,16 +1,52 @@
-import * as cdk from 'aws-cdk-lib/core';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const customersTable = new dynamodb.Table(this, 'CustomersTable', {
+      tableName: 'Customers',
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfrastructureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+      partitionKey: {
+        name: 'customerId',
+        type: dynamodb.AttributeType.STRING,
+      },
+
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    const ordersTable = new dynamodb.Table(this, 'OrdersTable', {
+      tableName: 'Orders',
+
+      partitionKey: {
+        name: 'orderId',
+        type: dynamodb.AttributeType.STRING,
+      },
+
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+
+      stream: dynamodb.StreamViewType.NEW_AND_OLD_IMAGES,
+
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    const inventoryTable = new dynamodb.Table(this, 'InventoryTable', {
+      tableName: 'Inventory',
+
+      partitionKey: {
+        name: 'sku',
+        type: dynamodb.AttributeType.STRING,
+      },
+
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
   }
 }
